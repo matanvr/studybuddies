ed5eb96b6be46396a6d867c65a0360125998d5f4
Matan Vardi <matanvr@gmail.com>
2014-03-01 18:14:19 -0800

    not allowing users to invite themselves

--- routes/class.js
+++ routes/class.js
@@ -11,6 +11,8 @@ exports.initialize = function(req, res) { 
 	var classData = req.query;
 	var output = {};
 	output.students = [];
+	output.user = data.Students[req.session.userID];
+
 	output.classInfo = classData;
 	for (var i = 0; i < data.Classes.length; i++){
 	
@@ -18,8 +20,13 @@ exports.initialize = function(req, res) { 
 			for (var s = 0; s < allClasses[i].students.length;s++){
 
 				for(var a = 0; a < allUsers.length; a++){	
-					console.log(allClasses[i].students[s]);
+					//console.log(allClasses[i].students[s]);
 					if(allClasses[i].students[s] && allClasses[i].students[s].id == allUsers[a].id){
+						if(allUsers[a].id == studentID)
+							allUsers[a].clickable = false;
+						else
+							allUsers[a].clickable = true;
+						
 						output.students.push(allUsers[a]);
 						break;
 					}
